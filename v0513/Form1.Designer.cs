48cb69774e285504bc10e56c3655137dd0149f2d
tanakaedu <tanakaedu@users.noreply.github.com>
2021-05-13 11:58:09 +0900

    ボタンを消す

--- v0513/Form1.Designer.cs
+++ v0513/Form1.Designer.cs
@@ -29,25 +29,31 @@ namespace v0513
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.mybutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // mybutton
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.mybutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.mybutton.Font = new System.Drawing.Font("UD デジタル 教科書体 N-B", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mybutton.Location = new System.Drawing.Point(0, 0);
+            this.mybutton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.mybutton.Name = "mybutton";
+            this.mybutton.Size = new System.Drawing.Size(200, 100);
+            this.mybutton.TabIndex = 0;
+            this.mybutton.Text = "ボタン";
+            this.mybutton.UseVisualStyleBackColor = false;
+            this.mybutton.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mybutton);
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Location = new System.Drawing.Point(5, 10);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,7 +62,7 @@ namespace v0513
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button mybutton;
     }
 }
 
